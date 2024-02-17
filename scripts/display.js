38c33d93b7dba0ec7cb74cd99d12ecaea516613f
taylynnharman <dyer.taylynn@gmail.com>
2024-02-17 14:05:14 -0700

    Fix image

--- scripts/display.js
+++ scripts/display.js
@@ -23,7 +23,7 @@ function createProjectCard(project) {
 
   //   // Create a thumbnail preview of project
   const image = document.createElement("img");
-  image.src = project.image;
+  image.src = "/Portfolio/" + project.image;
   image.alt = project.name;
   image.loading = "lazy";
   image.width = 100;
