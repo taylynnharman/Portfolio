7f9095333fdcd1055acf90e844590010a4400ae6
taylynnharman <dyer.taylynn@gmail.com>
2024-02-20 15:38:55 -0700

    Update

--- scripts/display.js
+++ scripts/display.js
@@ -43,7 +43,7 @@ function createProjectCard(project) {
 
   // Create Button to go to Project
   const button = document.createElement("a");
-  button.textContent = "View";
+  button.textContent = "VIEW";
   button.href = project.url;
   button.target = "_blank";
   projectCard.appendChild(title);
